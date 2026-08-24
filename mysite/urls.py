498912d0c0961528fd3c3890fe45137872fafb54
thowind <thomas.windischmann@gmx.de>
2026-08-24 13:42:13 +0200

    import von debug_tools entfernt

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
